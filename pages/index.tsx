767c24e159e810444e606d36c4ae4c01cae47a6b
Saad Heisentech <saadsiddiqui@Saads-MacBook-Air.local>
2024-02-04 02:12:07 +0530

    add: gradient background to home route

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,23 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="md:max-w-6xl flex-1 md:mx-auto">
-      <Head>
-        <title>Saad Siddiqui | Software Developer</title>
-        <link rel="icon" href="/react-logo.png" sizes="any" />
-      </Head>
-      <main className="md:mt-20">
-        <Header />
-        <HeroSection />
-      </main>
+    <div
+      className="flex-1  w-full h-screen bg-gradient-to-r
+    dark:from-gray-900
+    dark:via-gray-800
+    dark:to-neutral-900
+    background-animate"
+    >
+      <div className="md:max-w-6xl flex-1   md:mx-auto">
+        <Head>
+          <title>Saad Siddiqui | Software Developer</title>
+          <link rel="icon" href="/react-logo.png" sizes="any" />
+        </Head>
+        <main className="pt-5 md:pt-20">
+          <Header />
+          <HeroSection />
+        </main>
+      </div>
     </div>
   );
 }
