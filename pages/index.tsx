a4172a1df4e3390207536a624698ed9c1c9f380d
Saad Heisentech <saadsiddiqui@Saads-MacBook-Air.local>
2024-02-04 02:32:08 +0530

    change: main page stylings

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,23 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/hero-section";
 import Head from "next/head";
 
+//   className="flex-1  w-full h-screen bg-gradient-to-r
+// dark:from-gray-900
+// dark:via-gray-800
+// dark:to-neutral-900
+// background-animate"
+
 export default function Home() {
   return (
-    <div
-      className="flex-1  w-full h-screen bg-gradient-to-r
-    dark:from-gray-900
-    dark:via-gray-800
-    dark:to-neutral-900
-    background-animate"
-    >
-      <div className="md:max-w-6xl flex-1   md:mx-auto">
-        <Head>
-          <title>Saad Siddiqui | Software Developer</title>
-          <link rel="icon" href="/react-logo.png" sizes="any" />
-        </Head>
-        <main className="md:pt-20">
-          <Header />
-          <HeroSection />
-        </main>
-      </div>
+    <div className="md:max-w-6xl flex-1  md:mx-auto">
+      <Head>
+        <title>Saad Siddiqui | Software Developer</title>
+        <link rel="icon" href="/react-logo.png" sizes="any" />
+      </Head>
+      <main className="md:mt-20">
+        <Header />
+        <HeroSection />
+      </main>
     </div>
   );
 }
