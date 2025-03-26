1cb4d36240938dd718d518330be29a5e1224a864
saadsiddiqui07 <saadsiddiqui927@gmail.com>
2025-03-26 12:46:34 +0530

    move portfolio info in single json

--- components/hero-section/index.tsx
+++ components/hero-section/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ArrowRight from "../icons/ArrowRight";
 import Link from "next/link";
-import TwitterIcon from "../icons/TwitterIcon";
-import GithubIcon from "../icons/GithubIcon";
-import LinkedinIcon from "../icons/LinkedinIcon";
 import poppins from "@/styles";
+import { PORTFOLIO } from "@/data";
 
 const HeroSection = () => {
   return (
@@ -12,70 +10,52 @@ const HeroSection = () => {
       <div className="flex flex-col mt-20 mx-5 md:mt-30 sm:mx-10 md:mx-20">
         <div className="items-center">
           <h1 className="text-4xl sm:text-5xl md:text-6xl my-4 text-left font-extrabold">
-            Saad Siddiqui
+            {PORTFOLIO.title}
           </h1>
           <h5 className="text-lg font-semibold mt-2 mb-1 text-black dark:text-white">
-            Software Developer at {" "}
+            {PORTFOLIO.company.role} at{" "}
             <a
-              href="https://studiofrontier.com"
+              href={PORTFOLIO.company.link}
               target="_blank"
               rel="noopener noreferrer"
               className="underline text-black dark:text-white"
             >
-              Studio Frontier
+              {PORTFOLIO.company.name}
             </a>
           </h5>
           <span className="text-md text-gray-900 mt-2 sm:mt-0 dark:text-gray-400">
-           Expertise in building scalable, high-performance applications for web and mobile.
+            {PORTFOLIO.expertise}
           </span>
           <div className="flex items-start md:items-center mt-20 flex-col md:flex-row">
             <div className="mt-8 md:mt-0 mx-0 md:mx-6 space-y-2 dark:text-white">
-              <Link className="flex items-center" href="/blogs">
-                <ArrowRight />
-                <b>My Blogs</b>
-              </Link>
-              <Link className="flex items-center" href="/experience">
-                <ArrowRight />
-                <b>Work Experience</b>
-              </Link>
-              <Link className="flex items-center" href="/work">
-                <ArrowRight />
-                <b>My Work</b>
-              </Link>
-              {/* <Link className="flex items-center" href="/contact">
-                <ArrowRight />
-                <b>Contact Me</b>
-              </Link> */}
+              {PORTFOLIO.links.map((link, index) => (
+                <Link
+                  key={index}
+                  className="flex items-center"
+                  href={link.link}
+                >
+                  <ArrowRight />
+                  <b>{link.name}</b>
+                </Link>
+              ))}
             </div>
             <div className="flex-1 bg-yellow-400"></div>
             <div className="mt-8 md:mt-0 mx-0 md:mx-6 space-y-2 text-neutral-900 dark:text-neutral-200">
-              <a
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://twitter.com/saadsiddiquidev"
-                className="flex items-center gap-2"
-              >
-                <TwitterIcon />
-                <span className="font-bold">Twitter</span>
-              </a>
-              <a
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://github.com/saadsiddiqui07"
-                className="flex items-center gap-2"
-              >
-                <GithubIcon />
-                <span className="font-bold">Github</span>
-              </a>
-              <a
-                rel="noopener noreferrer"
-                target="_blank"
-                className="flex items-center gap-2"
-                href="https://www.linkedin.com/in/saad-siddiqui-0867391a3/"
-              >
-                <LinkedinIcon />
-                <span className="font-bold">LinkedIn</span>
-              </a>
+              {PORTFOLIO.socials.map((social, index) => {
+                const Icon = social.icon;
+                return (
+                  <a
+                    key={index}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href={social.link}
+                    className="flex items-center gap-2"
+                  >
+                    <Icon />
+                    <span className="font-bold">{social.name}</span>
+                  </a>
+                );
+              })}
             </div>
           </div>
         </div>
---
+++ data/index.ts
@@ -0,0 +1,45 @@
+import TwitterIcon from "../components/icons/TwitterIcon";
+import GithubIcon from "../components/icons/GithubIcon";
+import LinkedinIcon from "../components/icons/LinkedinIcon";
+
+
+export const PORTFOLIO = {
+    title: "Saad Siddiqui",
+    expertise: "Expertise in building scalable, high-performance applications for web and mobile.",
+    company: {
+        role: "Software Developer",
+        name: "Studio Frontier",
+        link: "https://studiofrontier.com",
+    },
+    links: [
+        {
+            name: "My Blogs",
+            link: "/blogs",
+        },
+        {
+            name: "Work Experience",
+            link: "/experience",
+        },
+        {
+            name: "My Work",
+            link: "/work",
+        },
+    ],
+    socials: [
+        {
+            name: "Twitter",
+            link: "https://twitter.com/saadsiddiquidev",
+            icon: TwitterIcon,
+        },
+        {
+            name: "Github",
+            link: "https://github.com/saadsiddiqui07",
+            icon: GithubIcon,
+        },
+        {
+            name: "LinkedIn",
+            link: "https://www.linkedin.com/in/saad-siddiqui-0867391a3/",
+            icon: LinkedinIcon,
+        },
+    ],
+};
